92e7884d9fe677a719ae68a6a33ba0bc4d12afdb
Clara260508 <148060242+Clara260508@users.noreply.github.com>
2025-03-06 11:12:55 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener(DOMContentLoaded, function() { 
+    const aumentafonte = document.getElementById('aumenta-fonte');
+
+    let fontsize = 16;
+
+    const diminuiFonteBotao = document.getElementById('diminui-fonte');
+})
